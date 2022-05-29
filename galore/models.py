c2196d9d8db9999f37fed47477c1dfcb2de41c72
Ngima-Muraguri <milcah.muraguri@student.moringaschool.com>
2022-05-30 02:47:31 +0300

    category models

--- galore/models.py
+++ galore/models.py
@@ -32,3 +32,52 @@ class Image(models.Model):
                 
             # result = cls.objects.filter(category__name__contains=category) #images assoc w/ this cat
             # return result
+
+    @classmethod
+    def filter_by_location(cls ,location):
+        '''
+        method to retrive images by their locations
+        '''
+        data = Image.objects.filter(location__city__contains=location)
+        return data
+    @classmethod
+    def get_image_by_id(cls, id):
+       
+        retrieved = Image.objects.get(id = id)
+        return retrieved
+    
+    def update_image(self, new_url):
+      
+        try:
+            self.image_link = new_url
+            self.save()
+            return self
+        except self.DoesNotExist:
+            print('Image entered does not exist.Please enter another image.')
+
+
+class Category(models.Model):
+    '''
+    model to handle category
+    '''
+    name = models.CharField(max_length=15)
+    
+    def __str__(self):
+        return self.name
+    
+    def save_category(self):
+        '''
+        method to save a category
+        '''
+        self.save()
+
+    def delete_category(self):
+        '''
+        method to delete a category
+        '''
+        self.delete()
+    def update_category(cls, search_term , new_cat):
+        '''
+        method to update a category
+        '''
+        try:
