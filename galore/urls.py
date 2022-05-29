646038e6307002d12c80477c2861f276ba78e5ed
Ngima-Muraguri <milcah.muraguri@student.moringaschool.com>
2022-05-30 02:38:36 +0300

    urls pattern

---
+++ galore/urls.py
@@ -0,0 +1,13 @@
+from django.urls import re_path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns=[
+    re_path('^$',views.gallery,name = 'gallery'),
+    re_path(r'^image/',views.display_image,name='displayImages'),
+    re_path(r'^search/', views.search_results, name='search_results'),
+    re_path(r'^photo/(\d+)',views.image,name ='image'),
+    ] 
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
