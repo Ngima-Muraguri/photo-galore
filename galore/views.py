b13fd2fc0f6b14a47d56a984cb5c97355c8e06fb
Ngima-Muraguri <milcah.muraguri@student.moringaschool.com>
2022-05-30 02:29:46 +0300

    created gallery view

--- galore/views.py
+++ galore/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import Http404
+from .models import Image,Category
+
 
 # Create your views here.
+def gallery(request):
+    return render(request, 'gallery.html')
+
+def display_image(request):
+    category = Category.objects.all()
+    image = Image.objects.all()
+    return render(request,'all-images.html',{'image':image,'category':category})
