ebe39582a77f5e6f22a6d637f282d9e835ad2c4c
Ngima-Muraguri <milcah.muraguri@student.moringaschool.com>
2022-05-30 02:31:17 +0300

    created search view

--- galore/views.py
+++ galore/views.py
@@ -11,3 +11,16 @@ def display_image(request):
     category = Category.objects.all()
     image = Image.objects.all()
     return render(request,'all-images.html',{'image':image,'category':category})
+
+def search_results(request):
+    if 'category' in request.GET and request.GET["category"]:
+        search_term = request.GET.get("category")
+        results = Image.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"images": results})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
+
