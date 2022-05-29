9c27b5ebe67a605d22424471e01926b1ddc5650d
Ngima-Muraguri <milcah.muraguri@student.moringaschool.com>
2022-05-30 02:43:17 +0300

    image model

--- galore/models.py
+++ galore/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    '''
+    model to handle images
+    '''
+    img = models.ImageField(upload_to='images/',default='1')
+    name = models.CharField(max_length =30)
+    description = models.TextField()
+    category = models.ForeignKey( 'Category', on_delete=models.CASCADE,default=1)
+    location = models.ForeignKey( 'Location', on_delete=models.CASCADE,default=1)
+
+    def __str__(self):
+        return self.
